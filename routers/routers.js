c4992cfb8593836caeb482be10f121329d749fb1
Isabel0426 <marisita2612@hotmail.com>
2022-07-29 20:36:20 -0500

    se sube analisis

--- routers/routers.js
+++ routers/routers.js
@@ -1,16 +1,9 @@
 const { Router} = require("express");
 const router = Router();
 const apiRoute = '/api';
-const studentsController= require('')
-const courseController = require('')
+const clinica= require('')
 
-//course
-router.get(apiRoute+'/course',courseController.getAll)
-router.post(apiRoute+'/course',courseController.create)
-router.put(apiRoute+'/course',courseController.update)
-router.delete(apiRoute+'/course',courseController.delete)
 
-//students
-router.get(apiRoute+'/pacientes',studentsController.getAll)
+router.get(apiRoute+'/pacientes',clinica.getAll)
 
 module.exports = router;
